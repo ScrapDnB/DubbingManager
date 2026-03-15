8be3f7fa126b4386ced51125d5e224e9460496cb
ScrapDnB <scrapdnb@icloud.com>
2026-03-15 20:35:57 +0300

    Апдейт информации о версии

--- ui/main_window.py
+++ ui/main_window.py
@@ -2146,7 +2146,7 @@ class MainWindow(QMainWindow):
             "О программе",
             "<h2>Dubbing Manager</h2>"
             "<p>Приложение для управления проектами дубляжа и озвучивания.</p>"
-            "<p><b>Версия:</b> 1.2</p>"
+            "<p><b>Версия:</b> 1.3</p>"
             "<p><b>GitHub:</b> <a href='https://github.com/ScrapDnB/DubbingManager/'>ScrapDnB/DubbingManager</a></p>"
             "<p><b>Python:</b> {}.{}.{}</p>"
             "<p><b>PySide6:</b> {}</p>"
